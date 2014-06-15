14cd6c57b143a9cbb5362da8865a0c70d8ed154f
C. Moss <caly.moss@gmail.com>
2014-06-15 14:16:27 -0700

    implemented collision detection and cleaned up code

--- watchout.js
+++ watchout.js
@@ -1,16 +1,24 @@
 // start slingin' some d3 here.
-// Create function to genenrate  random data points (positsions)
-//  produce an array
-// D3 functions
-// Create dots
-// make them show up
-//  d3 to the rescue  here
-//  These will deal with relationship between dots and data
-//  enter
-//  update
-//  exit
-//
-var coords = function () {
+gameStats = {
+	currentScore: 0,
+	highScore: 0,
+	collisions: 0
+}
+
+var updateCollisions = function() {
+	return d3.select('.collisions').text('Collisions: ' + gameStats.collisions.toString());
+};
+
+var updateScore = function() {
+	return d3.select('.current').text('Score: ' + gameStats.currentScore.toString());
+};
+
+var updateHighScore = function() {
+  gameStats.highScore = Math.max(gameStats.currentScore, gameStats.highScore);
+	return d3.select('.high').text('High score: ' + gameStats.highScore.toString());
+};
+
+var getCoordinates = function () {
   var dataArray = [];
 
   for (var i = 0; i < 50; i++) {
@@ -19,101 +27,83 @@ var coords = function () {
 	return dataArray;
 };
 
-var board = d3.select("body").append("svg").attr("width", window.innerWidth).attr("height", window.innerHeight);
+var board = d3.select("body").append("svg")
+		.attr("width", window.innerWidth)
+		.attr("height", window.innerHeight);
 
-var render = function(callback)   {
-  // var enemy = board.selectAll("circle").data(coords).enter().append("circle").attr("cx", function(d) {return d.x;}).attr("cy", function(d) { return d.y;}).attr("r", 10);
-  callback = coords || coords();
+var enemy = board.selectAll("circle.enemy").data(getCoordinates())
+		.enter().append("circle")
+		.attr("cx", function(d) {return d.x;})
+		.attr("cy", function(d) {return d.y;})
+		.attr("r", 10)
+		.attr("fill", "blue")
+		.attr("class", "enemy");
 
-  var enemy = board.selectAll("circle").data(coords);
+var move = function(coordinates) {
+	var newCoordinates = coordinates || getCoordinates();
+  enemy.data(newCoordinates)
+    .transition()
+    .duration(1000)
+    .attr("cx", function(d) {return d.x;})
+    .attr("cy", function(d) {return d.y;})
+  detectCollisions();
+  updateScore();
+  updateHighScore();
+}
+setInterval(move, 1000);
 
-  enemy.attr("cx", function(d) {return d.x;})
-  		.attr("cy", function(d) {return d.y;})
-  		.attr("r", 10);
 
-	enemy.enter().append("circle")
-			.attr("cx", function(d) {return d.x;})
-			.attr("cy", function(d) {return d.y;})
-			.attr("r", 10);
+var player = board.append("circle")
+	.attr("fill", "red")
+	.attr("class", "playerDot")
+	.attr("cx", Math.random() * window.innerWidth)
+	.attr("cy", Math.random() * window.innerHeight)
+	.attr("r", 10);
 
-  setTimeout(render, 1000);
+var dragMove = function() {
+	d3.select(this)
+		.attr("cy", d3.event.y)
+		.attr("cx", d3.event.x)
+  detectCollisions();
+}
+var drag = d3.behavior.drag().on("drag", dragMove);
+d3.select(".playerDot").call(drag);
+
+
+var intersects = function(x1, y1, r1, x2, y2, r2) {
+  var xDist = x1 - x2;
+  var yDist = y1 - y2;
+  var dist = Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+  return dist < r1 + r2;
 };
 
-// setInterval(function() {render(coords())}, 100)
 
-render();
+// So, detects collisions, but only gets called when dragging mouse, or (I
+// beleve) at the endpoint of a transition. Need to figure out how to do the
+// calculations *during* the transition, though that'll probably be
+// computationally intensive
+var detectCollisions = function() {
+  var collision = false;
+  enemy.each(function() {
+    var enemy = d3.select(this);
+    var enemyX = enemy[0][0].cx.animVal.value;
+    var enemyY = enemy[0][0].cy.animVal.value;
+    var enemyR = enemy[0][0].r.animVal.value;
+    var playerX = player[0][0].cx.animVal.value;
+    var playerY = player[0][0].cy.animVal.value;
+    var playerR = player[0][0].r.animVal.value;
+    if (intersects(enemyX, enemyY, enemyR, playerX, playerY, playerR)) {
+      gameStats.collisions++;
+      updateCollisions();
+      updateHighScore();
+      gameStats.currentScore = 0;
+      updateScore();
+    }
+  });
+};
 
-//*****************************
-
-// var boardWidth = window.innerWidth;
-// var boardHeight = window.innerHeight;
-
-// gameStats = {
-// 	currentScore: 0,
-// 	highScore: 0
-// }
-
-// //CVG container we'll attach enemies to
-// var svg = d3.select('body').append("svg")
-// 		.attr('width', boardWidth)
-// 		.attr('height', boardHeight);
-
-// //make enemies
-// var enemyCircles = d3.range(50).map(function() {return {
-// 	r: 10,
-// 	cx: Math.random() * innerWidth,
-// 	cy: Math.random() * innerHeight,
-// 	}
-// });
-
-// //make player
-
-// 	var player = svg.append("circle")
-// 			.attr("class", "player")
-// 			.attr("cx", enemyCircles[0].cx)
-// 			.attr("cy", enemyCircles[0].cy)
-// 			.attr("r", 10)
-// 			.attr("fill", "purple")
-
-// //place enemies every second
-// setInterval(function() {
-// 	for (var i = 1; i < enemyCircles.length; i++) {
-// 	svg.append('circle')
-// 		.attr("cx", enemyCircles[i].cx)
-// 		.attr("cy", enemyCircles[i].cy)
-// 		.attr("r", enemyCircles[i].r)
-// 		.style("fill", "green");
-//   }
-// }, 200);
-
-// //update the score
-// var updateScore = function() {
-// 	return d3.select('.current').text(gameStats.score.toString());
-// };
-
-// //update the high score
-// var updateScore = function() {
-// 	if (gameStats.score > gameStats.bestScore) {
-// 		gameStats.bestScore = gameStats.score;
-// 	}
-// 	return d3.select('.high').text(gameStats.highScore.toString());
-// };
-
-// //make a drag function
-// var dragMove = function(d) {
-// 	d3.select(this)
-// 		.attr("y", d3.event.y)
-// 		.attr("x", d3.event.x)
-// }
-
-// var drag = d3.behavior.drag();
-
-// d3.select(".player").call(drag);
-
-
-
-
-
-
-
-
+var increaseScore = function() {
+  gameStats.currentScore++;
+  updateScore();
+};
+setInterval(increaseScore, 50);
